de04fe516c6360247dfe45c95052941c878efaf3
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2021-03-24 09:26:57 -0400

    Added inheritance class simple example with getter and setter methods

--- ejemplo.php
+++ ejemplo.php
@@ -1,8 +1,9 @@
 <?php
+// Abstract class
 class Example
 {
     // Attribute
-    var $attribute;
+    public $attribute;
 
     // Constructor
     function Example()
@@ -17,27 +18,48 @@ class Example
     }
 
     // Method to define attribute value
-    function setAttributeValue($value)
+    function setAttribute($value)
     {
         $this->attribute = $value;
     }
 
-    function setAttribute($value)
-    {
-        $attribute = $value;
-    }
-
 }
 
-// Instantiate the class
-echo "Instantiate the class\n";
-$obj = new Example(0);
+// Object inheritance class
+class Example2 extends Example
+{
+    function Example2()
+    {
+        // Do nothing
+    }
 
-// Invoke the method
+    function getAttribute()
+    {
+        return $this->attribute;
+    }
+}
+
+// Instantiate the Abstract class
+echo "Instantiate the class\n";
+$obj = new Example();
+
+// Invoke the showName method
 $obj->showName('Leonardo Caballero');
 
-// Invoke the method to define attribute
-$obj->setAttributeValue(4);
-echo "The value of the attribute is " . $obj->attribute . "\n";
+// Invoke the setAttribute method to define attribute
+$obj->setAttribute(40);
+echo "The attribute value is " . $obj->attribute . "\n";
+
+// Instantiate Object inheritance class
+echo "\nInstantiate class for object inheritance\n";
+$obj2 = new Example2();
+
+// Invoke the showName method
+$obj2->showName('Ana Poleo');
+
+// Invoke the setAttribute method to define attribute
+$obj2->setAttribute(33);
+echo "The value of the attribute is " . $obj2->attribute . "\n";
+echo "Access to attribute value via a get method and return his value: " . $obj2->getAttribute() . "\n";
 ?>
 
