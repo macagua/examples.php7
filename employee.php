bb80a37baafe920ba932cd2b2d77f49d9031b898
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2021-03-24 18:27:05 -0400

    Added more comments to source code

--- employee.php
+++ employee.php
@@ -5,14 +5,17 @@
  */
 class Person
 {
+    // Class attributes
     protected $name;
     protected $age;
 
+    // Getter method for the Person full name
     public function getName()
     {
         return $this->name;
     }
 
+    // Setter method for the Person full name
     public function setName($name)
     {
         $this->name = $name;
@@ -35,56 +38,67 @@ class Person
  */
 class Employee extends Person
 {
+    // Class attributes
     private $designation;
     private $salary;
 
+    // Getter method for the Employee age
     public function getAge()
     {
         return $this->age;
     }
 
+    // Setter method for the Employee age
     public function setAge($age)
     {
         $this->age = $age;
     }
 
+    // Getter method for the Employee designation
     public function getDesignation()
     {
         return $this->designation;
     }
 
+    // Setter method for the Employee designation
     public function setDesignation($designation)
     {
         $this->designation = $designation;
     }
 
+    // Getter method for the Employee salary
     public function getSalary()
     {
         return $this->salary;
     }
- 
+
+    // Setter method for the Employee salary
     public function setSalary($salary)
     {
         $this->salary = $salary;
     }
 
+    // Getter method for the Employee name and age
     public function getNameAndAge()
     {
         return $this->callToProtectedNameAndAge();
     }
 }
 
+// Instantiate Object inheritance class
 $employee = new Employee();
 
-$employee->setName('Bob Smith');
-$employee->setAge(30);
+// Set the Employee attributes values
+$employee->setName('Leonardo Caballero');
+$employee->setAge(40);
 $employee->setDesignation('Software Engineer');
 $employee->setSalary('30K');
 
-echo $employee->getName() . "\n"; // prints 'Bob Smith'
-echo $employee->getAge() . "\n"; // prints '30'
+// Get the Employee attributes values
+echo $employee->getName() . "\n"; // prints 'Leonardo Caballero'
+echo $employee->getAge() . "\n"; // prints '40'
 echo $employee->getDesignation() . "\n"; // prints 'Software Engineer'
 echo $employee->getSalary() . "\n"; // prints '30K'
-echo $employee->getNameAndAge() . "\n"; // prints 'Bob Smith is 30 years old.'
+echo $employee->getNameAndAge() . "\n"; // prints 'Leonardo Caballero is 40 years old.'
 //echo $employee->callToPrivateNameAndAge(); // produces 'Fatal Error'
 
