625f7c3bda2d1b67f4e93deb649f132d87fd2d50
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2021-03-24 07:54:47 -0400

    Added initial PHP Class structure

---
+++ ejemplo.php
@@ -0,0 +1,15 @@
+<?php
+class Ejemplo
+{
+    // Constructor
+    function Ejemplo()
+    {
+       // Nada por aquí
+    }
+ 
+}
+ 
+// Inicie la clase
+echo "Iniciando la clase\n";
+$ob = new Ejemplo();
+?>
