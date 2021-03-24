4d35e64340552faecc32af122a8c788cf8354868
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2021-03-24 15:26:00 -0400

    Added a Person class example initial

--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 
 - [PHP 7 Orientado a Objetos](https://tutorialphp.net/php-7-orientado-a-objetos/).
 - [Aprende programación orientada a objetos (POO) con PHP desde cero](https://styde.net/aprende-programacion-orientada-a-objetos-poo-con-php/).
-
+- [Encapsulamiento, getters y setters en PHP](https://styde.net/encapsulamiento-y-uso-de-getters-y-setters-en-php/).
--- person.php
+++ person.php
@@ -1,37 +1,93 @@
 <?php
-// Show the full name
-function fullName($firstName, $lastName)
+/**
+ * Person Class
+ */
+
+// Abstract class
+class Person
 {
-    return "The name is {$firstName} {$lastName}\n";
+    // Class attributes
+    public $firstName;
+    public $lastName;
+    private $dateOfBirth;
+
+    // Class constructor method
+    public function __construct($firstName, $lastName, $dateOfBirth)
+    {
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+        $this->dateOfBirth = $dateOfBirth;
+    }
+
+    // Getter method for the Person full name
+    public function getFullName()
+    {
+        return $this->firstName . ' ' . $this->lastName;
+    }
+
+    // Setter method for the Person date of birth
+    public function setDateOfBirth($date)
+    {
+        $this->dateOfBirth = $date;
+    }
+
+    // Getter method for the Person date of birth
+    public function getDateOfBirth()
+    {
+        return $this->dateOfBirth;
+    }
+
+    // Getter method for calculate the Person age based on the dateOfBirth attribute
+    function getAge()
+    {
+        // Date in yyyy/mm/dd format; or it can be in other formats as well
+        // Explode the date to get year, month and day
+        $dob = explode("-", $this->dateOfBirth);
+        // Get age from date or birthdate
+        $age = (date("md", date("U", mktime(0, 0, 0, $dob[0], $dob[1], $dob[2]))) > date("md")
+        ? ((date("Y") - $dob[0]) - 1) 
+        : (date("Y") - $dob[0]));
+        return $age;
+    }
 }
 
-// Calculate age
-function age($dateOfBirth)
-{
-    // Date in yyyy/mm/dd format; or it can be in other formats as well
-    // Explode the date to get year, month and day
-    $dob = explode("-", $dateOfBirth);
+// Instantiate the Abstract class
+$ana = new Person("Ana", "Poleo", "1982-04-18");
+// Show show the attributes values
+echo "Name: " . $ana->firstName . ".\n";
+echo "Lastname: " . $ana->lastName . ".\n";
+// Get the Person full name
+echo "Fullname: " . $ana->getFullName() . ".\n";
+// Get the Person date of birth
+echo "Date of birth: " . $ana->getDateOfBirth() . ".\n";
+// Get the Person age
+echo "Age: " . $ana->getAge() . ".\n\n";
 
-    // Get age from date or birthdate
-    $age = (date("md", date("U", mktime(0, 0, 0, $dob[0], $dob[1], $dob[2]))) > date("md")
-    ? ((date("Y") - $dob[0]) - 1) 
-    : (date("Y") - $dob[0]));
-    return "The Age is: " . $age;
-}
+// Instantiate the Abstract class
+$leo = new Person("Leonardo", "Caballero", "1980-12-03");
+// Show show the attributes values
+echo "Name: " . $leo->firstName . ".\n";
+echo "Lastname: " . $leo->lastName . ".\n";
+// Get the Person full name
+echo "Fullname: " . $leo->getFullName() . ".\n";
+// Get the Person date of birth
+echo "Date of birth: " . $leo->getDateOfBirth() . ".\n";
+// Get the Person age
+echo "Age: " . $leo->getAge() . ".\n\n";
 
-echo fullName('Leonardo', 'Caballero');
-echo age('1980-12-03') . "\n";
-
-echo fullName('Ana', 'Poleo');
-echo age('1982-04-12') . "\n";
-
-$grandFather = array(
-    'firstName' => 'José Nicanor De Las Mercedes',
-    'lastName' => 'Ochoa Pinto Morillo',
-    'dateOfBirth' => '1868-02-19',
-);
-
-echo fullName($grandFather['firstName'], $grandFather['lastName']);
-echo age($grandFather['dateOfBirth']);
-?>
+// Set the Person First Name attribute value
+$leo->firstName = 'José Nicanor De Las Mercedes';
+// Set the Person Last Name attribute value
+$leo->lastName = 'Ochoa Pinto Morillo';
+// Invoke the Person setDateOfBirth method to define date of birth value
+$leo->setDateOfBirth('1868-02-19');
+// Show show the attributes values
+echo "Name: " . $leo->firstName . ".\n";
+echo "Lastname: " . $leo->lastName . ".\n";
+// Get the Person full name
+echo "Fullname: " . $leo->getFullName() . ".\n";
+// Get the Person date of birth
+echo "Date of birth: " . $leo->getDateOfBirth() . ".\n";
+// Get the Person age
+echo "Age: " . $leo->getAge() . ".\n\n";
 
