0423744184055c7418e000c116236a86ca2ed3d5
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2021-03-24 08:15:16 -0400

    Added attribute example fot this PHP Class

--- ejemplo.php
+++ ejemplo.php
@@ -1,16 +1,25 @@
 <?php
 class Ejemplo
 {
+    // Atributo
+    var $atributo;
+
     // Constructor
     function Ejemplo()
     {
-       // Nada por aquí
+        // Nada por aquí
     }
 
     // Método
     function MuestraNombre($nombre)
     {
-       echo "El nombre es " . $nombre . "\n";
+        echo "El nombre es " . $nombre . "\n";
+    }
+
+    // Método para definir atributo
+    function DaValor($valor)
+    {
+        $this->atributo = $valor;
     }
 
 }
@@ -22,4 +31,8 @@ $obj = new Ejemplo();
 // Invoque al método
 $obj->MuestraNombre('Leonardo Caballero');
 
+// Invoque al método para definir atributo
+$obj->DaValor(4);
+echo "El valor del atributo es " . $obj->atributo . "\n";
 ?>
+
