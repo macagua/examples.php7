13b0ceba10eb3481631fdeef8ae9f651616d6465
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2021-03-27 08:25:34 -0400

    Refactoring include file example

--- oop/greeting.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-include 'greet_func.php';
-
-echo greet('Leonardo', 'Visit https://plone.org') . "\n";
-
---
+++ oop/greeting_demo.php
@@ -0,0 +1,8 @@
+<?php
+
+// Include PHP Function
+include 'functions/greet.php';
+
+// Invoke the "greet" function to get the greeting message
+echo greet('Leonardo', 'Visit https://plone.org') . "\n";
+
