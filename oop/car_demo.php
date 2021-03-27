c7c951e9b3a440593ff772e59ad2568db235edbb
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2021-03-27 08:43:15 -0400

    Updated Car example with refactoring the source code

--- oop/car_demo.php
+++ oop/car_demo.php
@@ -1,42 +1,7 @@
 <?php
-/**
- * Car Class
- */
 
-class Car
-{
-    // Class attributes
-    var $brand;
-    var $model;
-    var $color;
-    var $doors;
-
-    // Class constructor method
-    function __construct($brand, $model, $color, $doors)
-    {
-        $this->brand = $brand;
-        $this->model = $model;
-        $this->color = $color;
-        $this->doors = $doors;
-    }
-
-    function carHorn()
-    {
-        return "Honking the car horn...";
-    }
-
-    function carSpeedUp()
-    {
-        return "Speeding the car...";
-    }
-
-    // Getter technical detail of car.
-    function technicalDetail()
-    {
-        return "Model " . $this->model . ", Color " . $this->color . ", Doors " . $this->doors . " and Brand " . $this->brand;
-    }
-
-}
+// Include PHP Class
+include 'classes/Car.php';
 
 $tesla = new Car('Tesla', 'S', 'Black', 4);
 // Show show the attributes values
---
+++ oop/classes/Car.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * Car Class
+ */
+class Car
+{
+    // Class attributes
+    var $brand;
+    var $model;
+    var $color;
+    var $doors;
+
+    // Class constructor method
+    function __construct($brand, $model, $color, $doors)
+    {
+        $this->brand = $brand;
+        $this->model = $model;
+        $this->color = $color;
+        $this->doors = $doors;
+    }
+
+    function carHorn()
+    {
+        return "Honking the car horn...";
+    }
+
+    function carSpeedUp()
+    {
+        return "Speeding the car...";
+    }
+
+    // Getter technical detail of car.
+    function technicalDetail()
+    {
+        return "Model " . $this->model . ", Color " . $this->color . ", Doors " . $this->doors . " and Brand " . $this->brand;
+    }
+
+}
+
